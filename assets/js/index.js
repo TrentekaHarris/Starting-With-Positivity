3f3530f1ed34e6d833645369f0ba9ef79bf5b173
TrentekaHarris <trentekaharris@gmail.com>
2022-12-06 13:15:39 -0500

    updated file to include javascript prompt

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,22 @@
 //variable for form
 const data = document.querySelector('#username');
 
+//Get desired name
+let nameValid = prompt("Please enter your name");
+console.log(nameValid)
 
-
-//Hide alert
-//document.getElementById("alert").style.display = "none";
 
 //ensure input has values alert
-function hasValue() {
-  let nameValid = document.forms["username"]["inputName"].value;
-  if(nameValid == "") {
-    alert("Please enter your desired name");
-    return false;
-  } else {
-    return getQuote() && getWeather();
-  }
-}
-
-//Get User's chosen name
- async function getUsername(form) {
-    var formData = new FormData(form);
-    /*console.log(Object.fromEntries(formData));
-    const choosenName = Object.fromEntries(formData);
-    document.getElementById("quote").innerHTML= `Hello ${choosenName}!
-    Here's some inspiration!`
-    console.log(choosenName)*/
-}
-
-//submit listener
-window.onload = function() {document.getElementById("username").addEventListener("submit", function (event) {
-    event.preventDefault();
-   getUsername(event.target);
-    hasValue();
+async function hasValue() {
   
- //hide name request
- document.getElementById("username").style.display = "none";
-})};
+  if(nameValid == null || nameValid == "") {
+    alert("Please enter your desired name");
+  return false} else {
+    return getQuote() && getWeather();
+}
+}
+
+hasValue()
 
 
 //get date
--- assets/js/weatherManager.js
+++ assets/js/weatherManager.js
@@ -1,31 +1,3 @@
-/*const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'df63d54958mshc6e5ae843b596aep179e42jsn21d36928056d',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-
-fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=userlocation&days=3', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-    const userlocation = navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        // Show a map centered at latitude / longitude.
-      });
-
-        fetch('https://extreme-ip-lookup.com/json/?key=GFhkPZYGjZdfppPIiJdn')
-      .then(res => res.json())
-      .then(response => {
-          location = response.city
-       })
-       .catch((data, status) => {
-          console.log('Request failed');
-       })*/
-
-
 //weather api
 const weather = {
         method: 'GET',
@@ -44,8 +16,8 @@ async function getWeather () {
 	
   .then(response => response.json())
     .then(response => {
-    document.getElementById("paraweather").innerHTML = `${response.location.name}  ${response.current.feelslike_f}°
-    ${response.current.condition.text}`;
+    document.getElementById("paraweather").innerHTML = `Hello ${nameValid} it is currently ${response.current.feelslike_f}° and
+    ${response.current.condition.text} in ${response.location.name}`;
      
   })
 }
